a91f1c8dd2a58aa03441744ea3d9a836cbad25b2
Pieter van de Bruggen <pvande@gmail.com>
2016-05-26 12:34:45 -0700

    Adding the `Channel` class.
    Reason: React does not presently (and may never) support context
    updates across `shouldComponentUpdate` boundaries.  This simple event
    emitting class will enable out-of-band communication from Container to
    Sticky children.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Sticky from './sticky';
 import Container from './container';
+import Channel from './channel';
 
 export { Sticky as Sticky };
 export { Container as StickyContainer };
+export { Channel as Channel };
 export default Sticky;
