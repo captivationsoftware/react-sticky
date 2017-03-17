0f82778e3ebc8272cb66747ed3e36b5f6994ad8a
Juul Rudihagen <juularthur92@gmail.com>
2017-03-17 10:18:48 +0100

    Run build with new changes to fix infinite scroll bug

--- dist/react-sticky.js
+++ dist/react-sticky.js
@@ -171,8 +171,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	    var _this = _possibleConstructorReturn(this, (Container.__proto__ || Object.getPrototypeOf(Container)).call(this, props));
 
 	    _this.updateOffset = function (_ref) {
-	      var inherited = _ref.inherited;
-	      var offset = _ref.offset;
+	      var inherited = _ref.inherited,
+	          offset = _ref.offset;
 
 	      _this.channel.update(function (data) {
 	        data.inherited = inherited + offset;
@@ -276,8 +276,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	    var _this = _possibleConstructorReturn(this, (Sticky.__proto__ || Object.getPrototypeOf(Sticky)).call(this, props));
 
 	    _this.updateContext = function (_ref) {
-	      var inherited = _ref.inherited;
-	      var node = _ref.node;
+	      var inherited = _ref.inherited,
+	          node = _ref.node;
 
 	      _this.containerNode = node;
 	      _this.setState({
@@ -370,6 +370,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	      var topBreakpoint = this.state.containerOffset - this.props.topOffset;
 	      var bottomBreakpoint = this.state.containerOffset + this.props.bottomOffset;
+	      if (fromBottom < 0) return this.state.isSticky;
 
 	      return fromTop <= topBreakpoint && fromBottom >= bottomBreakpoint;
 	    }
@@ -455,16 +456,15 @@ return /******/ (function(modules) { // webpackBootstrap
 	        style = _extends({}, style, _stickyStyle, this.props.stickyStyle);
 	      }
 
-	      var _props = this.props;
-	      var topOffset = _props.topOffset;
-	      var isActive = _props.isActive;
-	      var stickyClassName = _props.stickyClassName;
-	      var stickyStyle = _props.stickyStyle;
-	      var bottomOffset = _props.bottomOffset;
-	      var onStickyStateChange = _props.onStickyStateChange;
-	      var disablePlaceholder = _props.disablePlaceholder;
-
-	      var props = _objectWithoutProperties(_props, ['topOffset', 'isActive', 'stickyClassName', 'stickyStyle', 'bottomOffset', 'onStickyStateChange', 'disablePlaceholder']);
+	      var _props = this.props,
+	          topOffset = _props.topOffset,
+	          isActive = _props.isActive,
+	          stickyClassName = _props.stickyClassName,
+	          stickyStyle = _props.stickyStyle,
+	          bottomOffset = _props.bottomOffset,
+	          onStickyStateChange = _props.onStickyStateChange,
+	          disablePlaceholder = _props.disablePlaceholder,
+	          props = _objectWithoutProperties(_props, ['topOffset', 'isActive', 'stickyClassName', 'stickyStyle', 'bottomOffset', 'onStickyStateChange', 'disablePlaceholder']);
 
 	      return _react2.default.createElement(
 	        'div',
