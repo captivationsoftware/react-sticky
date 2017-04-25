ca59272fb4e0aef3934017a58b4a479bebcf80bd
Chris Graybeal <chrisgraybeal@Chriss-MacBook-Air.local>
2017-04-25 17:22:25 -0400

    StickyFooter

--- examples/basic/basic.js
+++ examples/basic/basic.js
@@ -9,7 +9,7 @@ class Document extends PureComponent {
     return (
       <div style={{ height: 1500, margin: '0 30px' }}>
         <h2>Content before the Sticky...</h2>
-        <div style={{ marginBottom: 200 }} />
+        <div style={{ marginBottom: 500 }} />
         <StickyContainer style={{ height: 500, background: '#ddd', padding: '0 30px' }}>
           <Sticky>
             {
@@ -26,10 +26,32 @@ class Document extends PureComponent {
             }
           </Sticky>
 
-          <h2 className="text-center" style={{ marginTop: 150 }}>&lt;StickyContainer /&gt;</h2>
+          <h2 className="text-center">&lt;StickyContainer /&gt;</h2>
         </StickyContainer>
         <div style={{ marginBottom: 200 }} />
           <h2>Content after the Sticky...</h2>
+          <div style={{ height: 200, marginTop: 200 }}/>
+          <StickyContainer style={{ height: 500, background: '#ddd', padding: '0 30px' }}>
+            <Sticky isFooter={true}>
+              {
+                ({ isSticky, wasSticky, style, distanceFromTop, distanceFromBottom, calculatedHeight }) => {
+                  console.log({ isSticky, wasSticky, style, distanceFromTop, distanceFromBottom, calculatedHeight });
+                  return (
+                    <div style={{ ...style, height: 80, overflow: 'auto', background: '#aaa'}}>
+                      <h2>
+                        <span className="pull-left">&lt;Sticky /&gt; <small>(invocation: #{i++})</small></span>
+                      </h2>
+                    </div>
+                  )
+                }
+              }
+            </Sticky>
+
+            <h2 className="text-center">&lt;StickyContainer /&gt;</h2>
+          </StickyContainer>
+          <div style={{ marginBottom: 100 }} />
+          <h2>Content after the Sticky...</h2>
+          <div style={{ height: 200, marginTop: 200 }}/>
         </div>
     );
   }
