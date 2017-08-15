901b906533c36a0d041db7b07555add348dfc3f4
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2017-08-15 19:44:28 +0300

    Fix relative top positioning

--- src/Sticky.js
+++ src/Sticky.js
@@ -67,7 +67,7 @@ export default class Sticky extends Component {
 
     const style = !isSticky ? { } : {
       position: 'fixed',
-      top: bottomDifference > 0 ? (this.props.relative ? parent.offsetTop - parent.offsetParent.scrollTop : 0) : bottomDifference,
+      top: bottomDifference > 0 ? (this.props.relative ? parent.getBoundingClientRect().top - parent.offsetParent.scrollTop : 0) : bottomDifference,
       left: placeholderClientRect.left,
       width: placeholderClientRect.width
     }
